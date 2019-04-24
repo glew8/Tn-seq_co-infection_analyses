22841af2b46e2b8d64ea378fc538026eb3bdfad0
Gina Lewin <glew8@users.noreply.github.com>
2019-04-24 16:13:29 -0400

    Update TnSeq2.sh
    Version of mapping script used by Gina Lewin (2019)

--- TnSeq2.sh
+++ TnSeq2.sh
@@ -1,13 +1,11 @@
-
 #!/usr/bin/env bash
+#This requires fqgrep (https://github.com/indraniel/fqgrep)
 usage () {
-  echo "usage: $0 [-i <IR seq>] [-g <path to genome files>] <pfx> "
+  echo "usage: $0 [-i <IR seq>] [-a <assembly>] <pfx> "
   echo "Required parameters:"
-  echo "-i     This is your IR sequence"
-  echo "-g     The location of the genome you're using (PA14)"
-  echo "must load these modules prior to using script: 
-        python/2.7, cutadapt/1.8.1, bowtie2/2.3.2"
-  echo "To load modules: module load python/2.7 , etc."
+  echo "-i     The sequence of the transposon end sequence remaining (for junction authentication)"
+  echo "-a     The name of the assembly you're using (PAO1)"
+  echo "-m     The number of mismatches/indels you want to tolerate during search"
 
   echo ""
   echo "The required parameters must precede the file prefix for your sequence file:"
@@ -15,14 +13,15 @@ usage () {
   echo "   the prefix is \"condition1\")"
   echo ""
   echo "Example:"
-  echo "$0 -i TATAAGAGTCAG -g $HOME/ref_genome/PA14/PA14 condition1"
+  echo "$0 -i TATAAGAGTCAG -a PAO1 -m 1 condition1"
 }
 
 # Read in the important options
-while getopts "i:g:" option; do
+while getopts ":i:a:m:" option; do
   case "$option" in
-          i)  IR="$OPTARG" ;;
-          g)  GENOME="$OPTARG" ;;
+  	i)  IR="$OPTARG" ;;
+  	a)  ASSEMBLY="$OPTARG" ;;
+  	m)  MISMATCHES="$OPTARG" ;;
     h)  # it's always useful to provide some help 
         usage
         exit 0 
@@ -46,8 +45,14 @@ if [ -z "$IR" ]; then
   exit 1
 fi
 
-if [ -z "$GENOME" ]; then
-  echo "Error: you must specify an assembly using -g"
+if [ -z "$ASSEMBLY" ]; then
+  echo "Error: you must specify an assembly using -a"
+  usage
+  exit 1
+fi
+
+if [ -z "$MISMATCHES" ]; then
+  echo "Error: you must specify a number of mismatches using -m"
   usage
   exit 1
 fi
@@ -60,40 +65,44 @@ if [ $# -lt 1 ] ; then
 fi
 
 PREFIX=$1
-BOWTIEREF=$GENOME
+R1=${PREFIX}
+BOWTIEREF=$REFGENOME$ASSEMBLY/$ASSEMBLY
 
 echo "Performing TnSeq analysis on $PREFIX..."
 echo "TnSeq processing stats for $PREFIX" > $PREFIX-TnSeq.txt
 echo "Total sequences: " >> $PREFIX-TnSeq.txt
-egrep -c '^@' $PREFIX.fastq >> $PREFIX-TnSeq.txt
-
+egrep -c '^@HWI|^@M|^@NS|^@SRR|^@NB5|^@M0' $R1.fastq >> $PREFIX-TnSeq.txt
 
 # IRs
-echo "$PREFIX: Searching for reads with an IR..."
-echo "$PREFIX: Removing primer and IR sequences..."
-#Modify the -M flag depending on you sequence leght. This is currently based on 75 bp reads.
-cutadapt -g $IR -M 45 -m 20 --discard-untrimmed -j 16 -o $PREFIX.trim.fastq $PREFIX.fastq >$PREFIX.cutadapt_log.txt
+echo "$PREFIX: Searching for reads with an IR in right location..."
+fqgrep -m $MISMATCHES -r -p $IR $R1.fastq | awk -F "\t" '(($8 >= 33 && $8 <= 44) || $1=="read name")' | trimmer --5-prime > $PREFIX-IR-clip.fastq
+~/.local/bin/cutadapt --nextseq-trim=20 -a G{16} -m 20 $PREFIX-IR-clip.fastq > $PREFIX-IR-clip.trim.fastq 2> $PREFIX-cutadapt-report.txt
+mv $PREFIX-IR-clip.trim.fastq $PREFIX-IR-clip.fastq
+IRSFOUND=$(egrep -c '^@HWI|^@M|^@NS|^@SRR|^@NB5|^@M0' $PREFIX-IR-clip.fastq)
+echo "Processed sequences:" >> $PREFIX-TnSeq.txt
+echo $IRSFOUND >> $PREFIX-TnSeq.txt
 
 # Map and convert - feel free to change bowtie2 parameters yourself
 echo "$PREFIX: Mapping with Bowtie2..."
 echo "Bowtie2 report:" >> $PREFIX-TnSeq.txt
-bowtie2 --end-to-end -p 16 -a -x $GENOME -U $PREFIX.trim.fastq -S $PREFIX.sam 2>> $PREFIX-TnSeq.txt
+bowtie2 --end-to-end -p 16 -x $BOWTIEREF -U $PREFIX-IR-clip.fastq -S $PREFIX.sam 2>> $PREFIX-TnSeq.txt
 grep '^@' $PREFIX.sam > $PREFIX-mapped.sam
-cat $PREFIX.sam | grep -v '^@' | awk -F "\t" '(and($2, 0x4) != 0x4)' | sort -u -k1,1 >> $PREFIX-mapped.sam
-echo "Number of reads mapping at high enough score:" >> $PREFIX-TnSeq.txt
-cat $PREFIX-mapped.sam | wc -l >> $PREFIX-TnSeq.txt
+cat $PREFIX.sam | grep -v '^@' | awk -F "\t" '((and($2, 0x4) != 0x4) && ($5 > 39))' >> $PREFIX-mapped.sam
+echo "Number of reads mapping at high enough MAPQ:" >> $PREFIX-TnSeq.txt
+grep -v '^@' $PREFIX-mapped.sam | wc -l >> $PREFIX-TnSeq.txt
 echo "$PREFIX: Tallying mapping results..."
-grep -v '^@' $PREFIX-mapped.sam | awk -F "\t" 'and($2, 0x100) != 0x100 {if (and($2, 0x10) != 0x10) print $4; else print $4+length($10)}' | grep '[0-9]' | sort | uniq -c | sort -n -r > $PREFIX-sites.txt
+grep -v '^@' $PREFIX-mapped.sam | awk -F "\t" '{if (and($2, 0x10) != 0x10) print $4; else print $4+length($10)+2}' | grep '[0-9]' | sort | uniq -c | sort -n -r > $PREFIX-sites.txt
 echo "Number of insertion sites identified:" >> $PREFIX-TnSeq.txt
 wc -l $PREFIX-sites.txt >> $PREFIX-TnSeq.txt
 echo "Most frequent sites:" >> $PREFIX-TnSeq.txt
-head -10 $PREFIX-sites.txt >> $PREFIX-TnSeq.txt
+head $PREFIX-sites.txt >> $PREFIX-TnSeq.txt
 
 # Sort output, cleanup
 echo "$PREFIX: Cleaning up..."
-mkdir $PREFIX 2> /dev/null
-mv $PREFIX.cutadapt_log.txt $PREFIX/
-mv $PREFIX-TnSeq.txt $PREFIX/
-mv $PREFIX.sam $PREFIX/
-mv $PREFIX-mapped.sam $PREFIX/
-mv $PREFIX-sites.txt $PREFIX/
+mkdir $PREFIX-results
+mv $PREFIX-TnSeq.txt $PREFIX-results/
+rm $PREFIX-IR-clip.fastq
+mv $PREFIX.sam $PREFIX-results/
+mv $PREFIX-mapped.sam $PREFIX-results/
+mv $PREFIX-sites.txt $PREFIX-results/
+mv $PREFIX-cutadapt-report.txt $PREFIX-results/
